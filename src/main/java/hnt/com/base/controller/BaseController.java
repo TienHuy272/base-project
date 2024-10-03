a6cb94dafa96e5a0f57a2e3e8d1b171d6bfa9d07
HuyNT <tienhuylionel@gmail.com>
2024-10-03 21:59:03 +0700

    add profile dev

---
+++ src/main/java/hnt/com/base/controller/BaseController.java
@@ -0,0 +1,16 @@
+package hnt.com.base.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class BaseController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        log.info("Someone ping me !!!");
+        return "pong";
+    }
+}
