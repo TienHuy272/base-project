cf62334362fa6a4a043830cc75cd7aaf3d11e432
HuyNT <tienhuylionel@gmail.com>
2024-10-20 09:47:24 +0700

    add customer transform field

--- src/main/java/hnt/com/base/practice/subselect/ProductReport.java
+++ src/main/java/hnt/com/base/practice/subselect/ProductReport.java
@@ -3,13 +3,17 @@ package hnt.com.base.practice.subselect;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Data;
+import org.hibernate.annotations.ColumnTransformer;
+import org.hibernate.annotations.Formula;
 import org.hibernate.annotations.Immutable;
 import org.hibernate.annotations.Subselect;
+
+import java.math.BigDecimal;
 
 @Entity
 @Immutable
 @Subselect(
-        value = "select p.id as id, p.name as product_name, c.name as category_name from products p left join categories c on c.id = p.category_id"
+        value = "select p.id as id, p.name as product_name, c.name as category_name, p.price as price from products p left join categories c on c.id = p.category_id"
 )
 @Data
 public class ProductReport {
@@ -17,4 +21,14 @@ public class ProductReport {
     private Long id;
     private String productName;
     private String categoryName;
+    @Formula(
+            "(SELECT AVG(p.price) FROM products p WHERE p.category_id = 1)"
+    )
+    private BigDecimal averagePrice;
+
+    @ColumnTransformer(
+            read = "price * 2",
+            write = "? * 2.20462"
+    )
+    private BigDecimal transformPrice;
 }
